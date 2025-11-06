f89adadbfcdfb735029394c6428b216eeee5f373
sem-a <aleksey.i99@mail.ru>
2025-11-06 17:03:07 +0300

    Добавлен store

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { PrivateRoute } from './components/PrivateRoute';
 import { PublicRoute } from './components/PublicRoute';
-import { Offer } from './types/offer';
 import NotFound from './pages/404';
 import { FavoritesPage } from './pages/Favorites';
 import { LoginPage } from './pages/Login';
 import Main from './pages/Main';
 import { OfferPage } from './pages/Offer';
 
-interface Props {
-  offers: Offer[];
-  favorites: Offer[];
-}
-
 const isAuthorized = false;
 
-const App: React.FC<Props> = ({ offers, favorites }) => (
+const App: React.FC = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
-      <Route index element={<Main offers={offers} />} />
+      <Route index element={<Main />} />
       <Route element={<PrivateRoute isAuthorized={isAuthorized} />}>
-        <Route
-          path="/favorites"
-          element={<FavoritesPage favorites={favorites} />}
-        />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Route>
       <Route element={<PublicRoute isAuthorized={isAuthorized} />}>
         <Route path="/login" element={<LoginPage />} />
