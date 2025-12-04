8142c529ec7643b4f4b9f6bc2fd9230b78c835eb
aasemikov <aleksey.i99@mail.ru>
2025-12-04 20:15:51 +0300

    fix bugs

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
 
-const isAuthorized = false;
-
 export const Layout = () => (
   <div className="page">
-    <Header isAuthorized={isAuthorized} />
+    <Header />
     <Outlet />
   </div>
 );
